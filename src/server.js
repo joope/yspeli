f9bf5704362f3523c99cc671fb429f62f080c305
joope <pyry.joona@helsinki.fi>
2018-03-19 23:31:17 +0200

    Add remote controlled object

---
+++ src/server.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/static/client.html');
+});
+
+app.get('/host', function(req, res){
+  res.sendFile(__dirname + '/static/host.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('control', function(control){
+    console.log(control)
+    io.emit('control_event', control)
+  })
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
